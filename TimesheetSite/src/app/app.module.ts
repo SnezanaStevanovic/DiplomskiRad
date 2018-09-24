c4d90a9ae20eeed2e6d2e17e7a538b083ee3879a
Vladimir Čović <vladimir.covic.kuzmi@gmail.com>
2018-09-25 00:04:06 +0200

    material added, user service added, folder organization

--- TimesheetSite/src/app/app.module.ts
+++ TimesheetSite/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './Components/Login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
